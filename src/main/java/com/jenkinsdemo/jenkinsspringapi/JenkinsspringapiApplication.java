76e92975fed76514dd3c1f8b07844f3802f20648
harshil <bonrixgpsemployeetracking@gmail.com>
2025-06-13 16:54:14 +0530

    Initial commit

---
+++ src/main/java/com/jenkinsdemo/jenkinsspringapi/JenkinsspringapiApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsdemo.jenkinsspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsspringapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsspringapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinsdemo/jenkinsspringapi/JenkinsspringapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsdemo.jenkinsspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsspringapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
